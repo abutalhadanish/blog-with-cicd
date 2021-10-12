03b12efe556fd41dc78ff8a6080ac89bd9f564a4
Talha <abutalhadanish@gmail.com>
2021-10-13 04:18:38 +0530

    Removed the errror

--- app/views.py
+++ app/views.py
@@ -59,8 +59,7 @@ class ProfileView(TemplateView):
 class SinglePostView(TemplateView):
     template_name = 'pages/single.html'
     
-    def get_context_data(self, **kwargs):
-        z=1/0
+    def get_context_data(self, **kwargs): #EEEEE
         blog = get_object_or_404(Blog, is_active=True, pk=kwargs['pk'])
         return {'blog': blog}
     
